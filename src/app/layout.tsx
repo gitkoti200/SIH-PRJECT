c4a2f140501e5a76a11337c610adbee8a7503589
Koteeswaran A <koteeswaran259@gmail.com>
2025-09-19 14:26:17 +0000

    remove the animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from '@/components/theme-provider';
-import { PageTransition } from '@/components/page-transition';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -34,9 +33,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <PageTransition>
             {children}
-          </PageTransition>
           <Toaster />
         </ThemeProvider>
       </body>
